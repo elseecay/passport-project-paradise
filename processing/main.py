f75ce3617b21b42e8d84ac7cde4a29632429ecbd
Aleksey Verkholat <elseecay@gmail.com>
2020-10-04 01:13:33 +1000

    auto capture and recognition

--- processing/main.py
+++ processing/main.py
@@ -1,48 +1,47 @@
 import sys
 import multiprocessing.shared_memory
 import win32event
-from procedures import INVALID_PROCEDURE_RESPONSE, PROCEDURES
+import procedures
 
 
-if len(sys.argv) != 5:
-    print('Usage: python main.py [mem][main_event][response_event][kill_event]')
+if len(sys.argv) != 6:
+    print('Usage: python main.py [mem][passport][recognized][result_taken][kill]')
     exit(0)
 
 
-def handle(index, buf):
-    proc = PROCEDURES.get(index, None)
-    if not proc:
-        MEM_BUF[:len(INVALID_PROCEDURE_RESPONSE)] = INVALID_PROCEDURE_RESPONSE
-        return
-    proc(buf)
-
-
 MEM_NAME = sys.argv[1]
-MAIN_EVENT_NAME = sys.argv[2]
-RESPONSE_EVENT_NAME = sys.argv[3]
-KILL_EVENT_NAME = sys.argv[4]
-MEM = multiprocessing.shared_memory.SharedMemory(MEM_NAME, False)
+PASSPORT_EVENT_NAME = sys.argv[2]
+RECOGNIZED_EVENT_NAME = sys.argv[3]
+RESULT_TAKEN_EVENT_NAME = sys.argv[4]
+KILL_EVENT_NAME = sys.argv[5]
+MEM = multiprocessing.shared_memory.SharedMemory(MEM_NAME, True, int(1e+7))
 MEM_BUF = MEM.buf
-MAIN_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, MAIN_EVENT_NAME)
-RESPONSE_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, RESPONSE_EVENT_NAME)
+PASSPORT_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, PASSPORT_EVENT_NAME)
+RECOGNIZED_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, RECOGNIZED_EVENT_NAME)
+RESULT_TAKEN_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, RESULT_TAKEN_EVENT_NAME)
 KILL_EVENT = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS, False, KILL_EVENT_NAME)
 
 
 while True:
-    waiting_result = win32event.WaitForSingleObject(MAIN_EVENT, 2000)
-    if waiting_result == win32event.WAIT_TIMEOUT:
-        waiting_result = win32event.WaitForSingleObject(KILL_EVENT, 500)
-        if waiting_result == win32event.WAIT_OBJECT_0:
-            print('Kill event has been received')
-            break
+    w = win32event.WaitForSingleObject(KILL_EVENT, 100)
+    if w == win32event.WAIT_OBJECT_0:
+        break
+    img = procedures.get_webcam_image()  # exception
+    if not procedures.is_passport(img):
         continue
-    handle(MEM_BUF[0], MEM_BUF)
-    win32event.SetEvent(RESPONSE_EVENT)
+    win32event.SetEvent(PASSPORT_EVENT)
+    data = procedures.recognition(img)  # exception
+    win32event.SetEvent(RECOGNIZED_EVENT)
+    procedures.json_to_mem(data)
+    w = win32event.WaitForSingleObject(RESULT_TAKEN_EVENT, 10000)
+    if w == win32event.WAIT_TIMEOUT:
+        print('Missing recognized passport, data:\n')  # TODO: recognized result
 
 
 MEM.close()
-MAIN_EVENT.close()
-RESPONSE_EVENT.close()
+PASSPORT_EVENT.close()
+RECOGNIZED_EVENT.close()
+RESULT_TAKEN_EVENT.close()
 KILL_EVENT.close()
 
 
